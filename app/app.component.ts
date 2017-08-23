446e16731205ca22507b91add71e781fba8c3421
Carlos Augusto Silveira de Oliveira <cardevisi@gmail.com>
2017-08-22 21:32:03 -0300

    [MASTER] cleanup project estructure with dist folder

--- app/app.component.ts
+++ app/app.component.ts
@@ -3,13 +3,42 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'my-app',
   template: `
+    
+    <header class="text-center">
+      <nav>
+        <div class="navbar-header">
+          <a href="/" class="navbar-brand">My Angular 2 App!</a>
+        </div>
+      </nav>
+    </header>
+
     <div class="jumbotron">
       <h1>Welcome to Our App!</h1>
+      <p>{{message}}</p>
     </div>
+
+    <div *ngIf="users">
+      <div *ngFor="let user of users">
+        {{user.name}} {{user.password}}
+      </div>
+    </div>
+    
+    <footer class="text-center">
+      Copyright &copy; 2016
+    </footer>
+
   `,
   styles: [`
     .jumbotron { box-shadow: 0 2px 0 rgba(0, 0, 0, 0.2); }
   `]
 })
 
-export class AppComponent {}
+export class AppComponent {
+	message = 'Hello';
+  users = [
+      {id: 10, name: 'Charles', username: 'servilcha'},
+      {id: 25, name: 'Ester', username: 'servilester'},
+      {id: 30, name: 'Francis', username: 'servilfrancis'}
+    ]
+  };
+}
--- dist/app.component.js
+++ dist/app.component.js
@@ -5,18 +5,26 @@ var __decorate = (this && this.__decorate) || function (decorators, target, key,
     else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
     return c > 3 && r && Object.defineProperty(target, key, r), r;
 };
-exports.__esModule = true;
+Object.defineProperty(exports, "__esModule", { value: true });
 var core_1 = require("@angular/core");
 var AppComponent = /** @class */ (function () {
     function AppComponent() {
+        this.message = 'Hello';
+        this.users = [
+            { id: 10, name: 'Charles', username: 'servilcha' },
+            { id: 25, name: 'Ester', username: 'servilester' },
+            { id: 30, name: 'Francis', username: 'servilfrancis' }
+        ];
     }
     AppComponent = __decorate([
         core_1.Component({
             selector: 'my-app',
-            template: "\n    <div class=\"jumbotron\">\n      <h1>Welcome to Our App!</h1>\n    </div>\n  ",
+            template: "\n    \n    <header class=\"text-center\">\n      <nav></nav>\n    </header>\n\n    <div class=\"jumbotron\">\n      <h1>Welcome to Our App!</h1>\n      <p>{{message}}</p>\n    </div>\n\n    <div *ngIf=\"users\">\n      <div *ngFor=\"let user of users\">\n        {{user.name}} {{user.password}}\n      </div>\n    </div>\n    \n    <footer class=\"text-center\">\n      Copyright &copy; 2016\n    </footer>\n\n  ",
             styles: ["\n    .jumbotron { box-shadow: 0 2px 0 rgba(0, 0, 0, 0.2); }\n  "]
         })
     ], AppComponent);
     return AppComponent;
 }());
 exports.AppComponent = AppComponent;
+;
+//# sourceMappingURL=app.component.js.map
